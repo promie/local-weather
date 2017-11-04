334f2ebc5783c4cffd35953bba81e062fb28d9eb
Promie Yutasane <promie.yutasane@gmail.com>
2017-11-04 18:55:08 +1100

    API Working

--- js/weather.js
+++ js/weather.js
@@ -1,29 +1,55 @@
 
-
-//Conversions
-
-//Arrow function ES6
+//Test newtown lat and long;
+//https://fcc-weather-api.glitch.me/api/current?lat=-33.8998891&lon=151.1714736
 
 
+$(document).ready(function(){
+
+    const api = 'https://fcc-weather-api.glitch.me/api/current?';
+    let lat, lon;
+    
+    //Get current geo-location
+    if(navigator.geolocation){
+        
+        navigator.geolocation.getCurrentPosition(position =>{
+            lat = 'lat=' + position.coords.latitude;
+            lon = 'lon=' + position.coords.longitude;
+            getAPIInfo(lat,lon);
+        });
+    }else{
+        console.log('Geolocation is not supported by this browser');
+    }
+
+
+    //get API info
+    const getAPIInfo = (lat, lon)=>{
+
+        const urlString = `${api}${lat}&${lon}`;
+        let temp, cityName;
+
+        $.ajax({
+            url: urlString,
+            success: (function(result){
+                temp = result.main.temp;
+                cityName = result.name;
+                
+                $('#weather').html(`${temp}°C`);
+                $('#city').html(cityName);
+
+            })
+        });
+
+    }
 
 
 
-//Logic-Algorithm
 
-//Get API using $.ajax() methods
 
-//Coversion
+});
 
 const toFahrenheit = c => (c * 1.8) + 32;
 const toCelsius = f => (f - 32)/1.8;
 
-
-
-//Button Switching to fahrenheight
-
-var weather = document.getElementById('weather').value;
-
-console.log(weather);
 
 
 
