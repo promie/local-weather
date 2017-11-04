983e24e0be4879741e8611fca73b2ab1de5c027e
Promie Yutasane <promie.yutasane@gmail.com>
2017-11-04 17:12:03 +1100

    Updating Files

---
+++ js/weather.js
@@ -0,0 +1,40 @@
+
+
+//Conversions
+
+//Arrow function ES6
+
+
+
+
+
+//Logic-Algorithm
+
+//Get API using $.ajax() methods
+
+//Coversion
+
+const toFahrenheit = c => (c * 1.8) + 32;
+const toCelsius = f => (f - 32)/1.8;
+
+
+
+//Button Switching to fahrenheight
+
+var weather = document.getElementById('weather').value;
+
+console.log(weather);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
